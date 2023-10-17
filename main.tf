720248cd78ec46fe8ad438466dc9eee8e7c36538
Diego Carballeda <39389013+diegocarba99@users.noreply.github.com>
2023-10-17 16:30:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -187,3 +187,17 @@ resource "newrelic_synthetics_secure_credential" "service_2_api_key" {
   value = var.service_2_api_key
   account_id = local.account_id
 }
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
